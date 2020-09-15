44f0bca15c0a1795861d98dd3f9a76e6ab7b09e9
blacitea <angelhoikei@gmail.com>
2020-09-15 06:23:06 +0000

    add custom validation

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validate :carry_mens_or_womens #customs validate not validates
+
+  def carry_mens_or_womens
+    if !mens_apparel & !womens_apparel
+      errors.add(:mens_apparel, :womens_apparel, "Must carry at least one of the men's or women's apparel.")
+    end
+  end
+
 end
